19a9434d4ca80771603d40ae0d1648dc42709c3d
Patrick Dias <117788162+Patrick510@users.noreply.github.com>
2024-08-08 15:08:56 -0400

    :08-08: Modulo 1 Finalizado

---
+++ curso/modulo 1/aula1/desafio1/index.js
@@ -0,0 +1,6 @@
+var number1 = 10;
+var number2 = 15;
+function sumNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(sumNumbers(number1, number2));
---
+++ curso/modulo 1/aula1/desafio1/index.ts
@@ -0,0 +1,8 @@
+const number1 = 10;
+const number2 = 15;
+
+function sumNumbers(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log(sumNumbers(number1, number2));
